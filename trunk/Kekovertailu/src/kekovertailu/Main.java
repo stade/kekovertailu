c4751fd5647e55c9660c47783eef14ed8e40cbb5
tatu.tahvanainen <tatu.tahvanainen@190ba5e1-2c88-76a5-0b1f-ebf3ee0535e9>
2011-05-22 17:07:33 +0000

    Main luokan lisäys

---
+++ trunk/Kekovertailu/src/kekovertailu/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kekovertailu;
+
+/**
+ *
+ * @author tatutahv
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        MinBinaryheap heap = new MinBinaryheap();
+
+        heap.heapInsert(55);
+        heap.heapInsert(33);
+        heap.heapInsert(44);
+        heap.heapInsert(77);
+        heap.heapInsert(22);
+        heap.heapInsert(99);
+        heap.heapInsert(34);
+        heap.heapInsert(88);
+        heap.heapInsert(2);
+
+        int res;
+        while (heap.getHeapsize() != 0) {
+
+            res = heap.heapDelMin();
+
+            System.out.println(res);
+        }
+    }
+}
