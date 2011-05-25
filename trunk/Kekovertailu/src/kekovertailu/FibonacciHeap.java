0defcd29c0ecd0bb733d2c71653e59a6686242a0
tatu.tahvanainen <tatu.tahvanainen@190ba5e1-2c88-76a5-0b1f-ebf3ee0535e9>
2011-05-25 17:03:23 +0000

    Toteutettu Fibonaccikeko-luokkaan heapinsert-metodi ja sen vaatimat aksessorit FibonacciNode-luokkaan

--- trunk/Kekovertailu/src/kekovertailu/FibonacciHeap.java
+++ trunk/Kekovertailu/src/kekovertailu/FibonacciHeap.java
@@ -3,7 +3,9 @@
  * and open the template in the editor.
  */
 
+
 package kekovertailu;
+
 
 /**
  *
@@ -11,8 +13,75 @@ package kekovertailu;
  */
 public class FibonacciHeap {
 
-    
+    private FibonacciNode min;
+    private int numNodes;
 
+    public FibonacciHeap() {
 
+        this.min = null;
+        this.numNodes = 0;
+
+    }
+    public void heapInsert(int key) {
+
+        FibonacciNode node = new FibonacciNode(key);
+
+        
+
+        // If heap is empty node becames min
+        if (min == null) {
+            node.setLeft(node);
+            node.setRight(node);
+            min = node;
+            
+        }
+        else {
+            // If node is smaller than min it becames min
+            
+            // If min is the only node in the heap node becames left and right of min
+            if (min.getLeft().equals(min)) {
+
+                min.setLeft(node);
+                min.setRight(node);
+                node.setLeft(min);
+                node.setRight(min); 
+
+            }
+            // If there is more nodes than min in the heap, node becames left of min and right of
+            // the node that was left to min
+            else {
+                   
+                FibonacciNode temp = min.getLeft();
+
+                temp.setRight(node);
+                node.setLeft(temp);
+                node.setRight(min);
+                min.setLeft(node);
+
+            }
+            if (node.getKey() < min.getKey()) {
+
+                min = node;
+
+            }
+         } 
+        numNodes = numNodes + 1;
+    }
+    public void printRootList() {
+
+        int i = 0;
+        FibonacciNode temp = min;
+
+        while (i < numNodes) {
+
+           
+            System.out.println(temp.getKey());
+
+            temp = temp.getLeft();
+            i++;
+
+        }
+        
+    }
 
 }
--- trunk/Kekovertailu/src/kekovertailu/FibonacciNode.java
+++ trunk/Kekovertailu/src/kekovertailu/FibonacciNode.java
@@ -15,20 +15,46 @@ public class FibonacciNode {
     private FibonacciNode right;
     private FibonacciNode parent;
     private FibonacciNode child;
+    private int key;
     private int degree;
     private boolean mark;
 
     public FibonacciNode() {
 
-        left = null;
-        right = null;
-        parent = null;
-        child = null;
-        degree = 0;
-        mark = false;
+        this.key = 0;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+        this.child = null;
+        this.degree = 0;
+        this.mark = false;
     }
-
-    
-
+    public FibonacciNode(int k) {
+        this.key = k;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+        this.child = null;
+        this.degree = 0;
+        this.mark = false;
+    }
+    public void setDegree(int d) {
+        this.degree = d;
+    }
+    public void setLeft(FibonacciNode node) {
+        this.left = node;
+    }
+    public void setRight(FibonacciNode node) {
+        this.right = node;
+    }
+    public int getKey() {
+        return this.key;
+    }
+    public FibonacciNode getLeft() {
+        return this.left;
+    }
+    public FibonacciNode getRight() {
+        return this.right;
+    }
 
 }
